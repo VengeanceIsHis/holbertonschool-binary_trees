bae0bdc9b72e5839354e94293a58af1e2fd70fd2
VengeanceIsHis <elijahjpoole@gmail.com>
2023-12-22 19:34:25 -0800

    fix

--- binary_trees.h
+++ binary_trees.h
@@ -38,4 +38,5 @@ typedef struct levelorder_queue_s
 	struct levelorder_queue_s *next;
 } levelorder_queue_t;
 void binary_tree_print(const binary_tree_t *);
+typedef struct binary_tree_s binary_tree_t;
 #endif
